4e48f542032f948ec9efcc7dfd62afa2dfa7177d
mike <m@d>
2019-12-01 21:31:45 -0500

    add site attribute to links for future organization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent {
       subtitle: 'Reddit',
       avatarUrl: 'assets/reddit-icon.svg',
       links: [
-        { text: "entr", url: "https://www.reddit.com/r/Entrepreneur/" },
-        { text: "fi", url: "https://www.reddit.com/r/financialindependence/" }
+        { text: "entr", url: "https://www.reddit.com/r/Entrepreneur/", site: "Reddit" },
+        { text: "fi", url: "https://www.reddit.com/r/financialindependence/", site: "Reddit" }
       ]
     },
     {
@@ -24,8 +24,8 @@ export class AppComponent {
       subtitle: 'Easy watching',
       avatarUrl: 'assets/youtube-icon.svg',
       links: [
-        { text: "growth", url: "https://www.youtube.com/user/phuckmediocrity/videos" },
-        { text: "money", url: "https://www.youtube.com/channel/UCYT-aXiAB-s9xf6cFcXCnNg/videos" }
+        { text: "growth", url: "https://www.youtube.com/user/phuckmediocrity/videos", site: "YouTube" },
+        { text: "money", url: "https://www.youtube.com/channel/UCYT-aXiAB-s9xf6cFcXCnNg/videos", site: "YouTube" }
       ]
     },
     {
@@ -33,11 +33,11 @@ export class AppComponent {
       subtitle: 'Imageboards',
       avatarUrl: 'assets/clover-icon.svg',
       links: [
-        { text: "biz", url: "https://boards.4channel.org/biz/catalog" },
-        { text: "lit", url: "https://boards.4channel.org/lit/catalog" },
-        { text: "pol", url: "https://boards.4channel.org/pol/catalog" },
-        { text: "g", url: "https://boards.4channel.org/g/catalog" },
-        { text: "b", url: "https://boards.4channel.org/b/catalog" }
+        { text: "biz", url: "https://boards.4channel.org/biz/catalog", site: "Misc." },
+        { text: "lit", url: "https://boards.4channel.org/lit/catalog", site: "Misc." },
+        { text: "pol", url: "https://boards.4channel.org/pol/catalog", site: "Misc." },
+        { text: "g", url: "https://boards.4channel.org/g/catalog", site: "Misc." },
+        { text: "b", url: "https://boards.4channel.org/b/catalog", site: "Misc." }
       ]
     }
   ];
