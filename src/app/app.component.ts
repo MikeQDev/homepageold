95f4207ffbdea54d52662367c1ab47f65c7011ec
mike <m@d>
2019-12-01 21:37:32 -0500

    add category for organization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'homepage';
   sortBySite = true;
 
@@ -15,8 +15,8 @@ export class AppComponent {
       subtitle: 'Reddit',
       avatarUrl: 'assets/reddit-icon.svg',
       links: [
-        { text: "entr", url: "https://www.reddit.com/r/Entrepreneur/", site: "Reddit" },
-        { text: "fi", url: "https://www.reddit.com/r/financialindependence/", site: "Reddit" }
+        { text: "entr", url: "https://www.reddit.com/r/Entrepreneur/", site: "Reddit", category: "Business" },
+        { text: "fi", url: "https://www.reddit.com/r/financialindependence/", site: "Reddit", category: "Finance" }
       ]
     },
     {
@@ -24,8 +24,8 @@ export class AppComponent {
       subtitle: 'Easy watching',
       avatarUrl: 'assets/youtube-icon.svg',
       links: [
-        { text: "growth", url: "https://www.youtube.com/user/phuckmediocrity/videos", site: "YouTube" },
-        { text: "money", url: "https://www.youtube.com/channel/UCYT-aXiAB-s9xf6cFcXCnNg/videos", site: "YouTube" }
+        { text: "growth", url: "https://www.youtube.com/user/phuckmediocrity/videos", site: "YouTube", category: "Growth" },
+        { text: "money", url: "https://www.youtube.com/channel/UCYT-aXiAB-s9xf6cFcXCnNg/videos", site: "YouTube", category: "Finance" }
       ]
     },
     {
@@ -33,17 +33,21 @@ export class AppComponent {
       subtitle: 'Imageboards',
       avatarUrl: 'assets/clover-icon.svg',
       links: [
-        { text: "biz", url: "https://boards.4channel.org/biz/catalog", site: "Misc." },
-        { text: "lit", url: "https://boards.4channel.org/lit/catalog", site: "Misc." },
-        { text: "pol", url: "https://boards.4channel.org/pol/catalog", site: "Misc." },
-        { text: "g", url: "https://boards.4channel.org/g/catalog", site: "Misc." },
-        { text: "b", url: "https://boards.4channel.org/b/catalog", site: "Misc." }
+        { text: "biz", url: "https://boards.4channel.org/biz/catalog", site: "Misc.", category: "Business" },
+        { text: "lit", url: "https://boards.4channel.org/lit/catalog", site: "Misc.", category: "Time waster" },
+        { text: "pol", url: "https://boards.4channel.org/pol/catalog", site: "Misc.", category: "Time waster" },
+        { text: "g", url: "https://boards.4channel.org/g/catalog", site: "Misc.", category: "Time waster" },
+        { text: "b", url: "https://boards.4channel.org/b/catalog", site: "Misc.", category: "Time waster" }
       ]
     }
   ];
 
   toggleSortBySite(toggled) {
-    console.log(toggled);
+    this.sortBySite = toggled;
+    // this.linkCards = [];
   }
 
+  ngOnInit(): void {
+    this.toggleSortBySite(this.sortBySite);
+  }
 }
