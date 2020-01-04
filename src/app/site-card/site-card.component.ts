02f39de30baf0daae409a8f91d7b108b207b5f03
mike <m@d>
2020-01-04 10:17:46 -0500

    remove unnecessary var value

--- src/app/site-card/site-card.component.ts
+++ src/app/site-card/site-card.component.ts
@@ -10,21 +10,7 @@ export class SiteCardComponent {
   public horizontal = true;
 
   @Input()
-  public card: any = {
-    avatarUrl: "https://www.infragistics.com/angular-demos/assets/images/card/avatars/mellow_d.jpg",
-    links: [
-      { text: "google", url: "https://google.com/" },
-      { text: "reddit", url: "https://reddit.com/" },
-      { text: "google", url: "https://google.com/" },
-      { text: "reddit", url: "https://reddit.com/" },
-      { text: "google", url: "https://google.com/" },
-      { text: "reddit", url: "https://reddit.com/" }
-    ],
-    description: 'some description',
-    imageUrl: "https://www.infragistics.com/angular-demos/assets/images/card/media/here_media.jpg",
-    subtitle: "Subtitle",
-    title: "Title"
-  };
+  public card: any = {};
 
   newTab(e) {
     window.open(e.url);
